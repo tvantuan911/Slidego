a348d446412598c2b11f7ba1fdddb4a8c8cd148a
Tuan Truong <tvantuan911@gmail.com>
2025-04-14 22:59:45 +0700

    Update infinite loop

--- src/slidego.js
+++ src/slidego.js
@@ -14,9 +14,10 @@ function Slidego(selector, options = {}) {
         options
     );
     this.slides = Array.from(this.container.children);
-    this.currentIndex = 0;
+    this.currentIndex = this.opt.loop ? this.opt.items : 0;
 
     this._init();
+    this._updatePosition();
 }
 
 Slidego.prototype._init = function () {
@@ -28,6 +29,11 @@ Slidego.prototype._init = function () {
 Slidego.prototype._createTrack = function () {
     this.track = document.createElement("div");
     this.track.className = "slidego-track";
+
+    const cloneHead = this.slides.slice(-this.opt.items).map((node) => node.cloneNode(true));
+    const cloneTail = this.slides.slice(0, this.opt.items).map((node) => node.cloneNode(true));
+
+    this.slides = cloneHead.concat(this.slides.concat(cloneTail));
 
     this.slides.forEach((slide) => {
         slide.classList.add("slidego-slide");
@@ -55,10 +61,24 @@ Slidego.prototype._createNavigation = function () {
 };
 
 Slidego.prototype.moveSlide = function (step) {
+    if (this._isAnimating) return;
+    this._isAnimating = true;
+
     if (this.opt.loop) {
         this.currentIndex =
             (this.currentIndex + step + this.slides.length) %
             this.slides.length;
+
+        this.track.ontransitionend = () =>  {
+            const maxIndex = this.slides.length - this.opt.items;
+            if (this.currentIndex <= 0) {
+                this.currentIndex = maxIndex - this.opt.items;
+            } else if (this.currentIndex >= maxIndex) {
+                this.currentIndex = this.opt.items;
+            }
+            this._updatePosition(true);
+            this._isAnimating = false;
+        }
     } else {
         this.currentIndex = Math.min(
             Math.max(this.currentIndex + step, 0),
@@ -66,6 +86,14 @@ Slidego.prototype.moveSlide = function (step) {
         );
     }
     console.log(this.currentIndex);
+    this._updatePosition();
+};
+
+Slidego.prototype._updatePosition = function (instant = false) {
+    this.track.style.transition = instant ? 'none' : 'transform ease 0.3s';
     this.offset = -(this.currentIndex * (100 / this.opt.items));
     this.track.style.transform = `translateX(${this.offset}%)`;
-};
+}
+
+
+//  4 5 6 1 2 3 4 5 6 1 2 3 
